d8d1837a1441ca59526286184d7635f691d68161
PYL-Oceaguard <py.luminet@outlook.com>
2026-08-18 15:28:12 +0200

    Add files via upload
    mise a jour du 18/08

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,69 @@
-const monImage = document.querySelector("img");
+const loginButton = document.querySelector("#login-button");
 
-monImage.addEventListener("click", () => {
-  const maSrc = monImage.getAttribute("src");
-
-  if (maSrc === "images/Logo Titre OceaGuard.png") {
-    monImage.setAttribute("src", "images/representation-activite-oceaguard.png");
-  } else {
-    monImage.setAttribute("src", "images/Logo Titre OceaGuard.png");
-  }
+loginButton.addEventListener("click", function () {
+    alert("La connexion OCEAGuard sera bientôt disponible !");
 });
 
-let monBouton = document.querySelector("button");
+const temperature = document.querySelector("#temperature");
+const ph = document.querySelector("#ph");
+const oxygen = document.querySelector("#oxygen");
+const turbidity = document.querySelector("#turbidity");
+const turbidityStatus = document.querySelector("#turbidity-status");
+const salinity = document.querySelector("#salinity");
 
-function essaiconnexion() {
-  const monIdentifianr = prompt("Veuillez saisir votre identifiant");
-  localStorage.setItem("identifiant", minIdentifiant);
-  Bienvenue.textContent = `Bienvenue, ${monIdentifiant}`;
+const temperatureValue = 21.7;
+const phValue = 8.1;
+
+const oxygenValue = 7.8;
+  function analyserOxygene(valeur) {
+
+    if (valeur > 6) {
+        return "🟢 Normal";
+    } else if (valeur >= 3) {
+        return "🟠 Vigilance";
+    } else {
+        return "🔴 Alerte";
+    }
+
 }
+const oxygenStatus = document.querySelector("#oxygen-status");
+oxygenStatus.textContent = analyserOxygene(oxygenValue);
 
-monBouton.addEventListener("click", () => {
-  essaiconnexion();
-});
+const salinityValue = 34.2;
+
+const turbidityValue = 2.5;
+    function analyserTurbidite(valeur) {
+      if (valeur < 5) {
+          return "🟢 Normal";
+      } else if (valeur <= 10) {
+          return "🟠 Vigilance";
+      } else {
+          return "🔴 Alerte";
+      }
+    }
+
+    turbidityStatus.textContent = analyserTurbidite(turbidityValue);
+
+
+
+temperature.textContent = temperatureValue + " °C";
+ph.textContent = phValue;
+oxygen.textContent = oxygenValue + " mg/L";
+turbidity.textContent = turbidityValue + " NTU";
+salinity.textContent = salinityValue + " PSU";
+
+const statusMessage = document.querySelector(".status-message");
+
+if (temperatureValue < 10) {
+
+    statusMessage.textContent = "🟠 Température basse";
+
+} else if (temperatureValue > 25) {
+
+    statusMessage.textContent = "🔴 Attention : température élevée";
+
+} else {
+
+    statusMessage.textContent = "🟢 Toutes les mesures sont normales";
+
+}
